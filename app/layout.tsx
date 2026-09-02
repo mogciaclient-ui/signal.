a79137c5da965331aea657a56624cec75ea12985
mogciaclient-ui <mogcia.client@gmail.com>
2026-09-03 08:08:16 +0900

    Add Meta app review submission package

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./brand.css";
 import "./live-data.css";
+import "./review-ready.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
